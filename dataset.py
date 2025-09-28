4d4185e91ad9dc5d92c59709c3605b392c890dc6
Elane <muc.555@gmail.com>
2025-09-28 17:05:03 +0500

    add: load dataset

--- dataset.py
+++ dataset.py
@@ -1,2 +1,7 @@
-def printTest():
-	print("test")
+import kagglehub
+import shutil
+
+def download_dataset():
+  path2data = kagglehub.dataset_download("sartajbhuvaji/brain-tumor-classification-mri")
+  print("Path to dataset files:", path2data)
+  return path2data
